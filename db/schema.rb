ffa009ab8ceec60c6fff1bcb6d87f7c00a5fe4ca
Margaux Stanislas <margaux.stanislas@hotmail.fr>
2021-01-07 16:11:56 +0100

    model conv et message

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_130230) do
+ActiveRecord::Schema.define(version: 2021_01_07_145651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "host_id"
+    t.bigint "guest_id"
+    t.index ["guest_id"], name: "index_conversations_on_guest_id"
+    t.index ["host_id"], name: "index_conversations_on_host_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "conversation_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,8 @@ ActiveRecord::Schema.define(version: 2021_01_07_130230) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conversations", "users", column: "guest_id"
+  add_foreign_key "conversations", "users", column: "host_id"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
